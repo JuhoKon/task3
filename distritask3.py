d80266fba7103a6bb038e3d4f1845e959ceb5516
JuhoKon <kontiainen97@hotmail.fi>
2020-02-19 18:32:46 +0200

    Fix

--- distritask3.py
+++ distritask3.py
@@ -7,52 +7,35 @@ from bs4 import BeautifulSoup
 from collections import deque
 import pythonSlave
 import multiprocessing
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Value
 import asyncio
+from termcolor import colored
 
 
-def redirected(end):
-    '''
-    Returns the url that end page points to (helpful for end pages with redirected url)
-    '''
-    end_soup = BeautifulSoup(requests.get(end).content, 'html.parser')
-    title = end_soup.find('h1').text
-    title = title.replace(' ', '_', len(title))
-    base_url = end[:end.find('/wiki/') + len('/wiki/')]
-    return set([end, base_url + title])
-
-
-def get_links(page):
-    '''
-    Retrieves distinct links in a Wikipedia page.
-    '''
-    r = requests.get(page)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    base_url = page[:page.find('/wiki/')]
-    links = list({base_url + a['href']
-                  for a in soup.select('p a[href]') if a['href'].startswith('/wiki/')})
-    return links
-
-
-def processes(linksFirstPart, linksSecondPart, linksThirdPart, linksFourthPart, linksFifthPart, Q, start, end, return_dict, event):
+def processes(linksFirstPart, linksSecondPart, linksThirdPart, linksFourthPart, linksFifthPart, Q, start, end, return_list, run):
     p1 = Process(target=pythonSlave.find_shortest_path,
-                 args=(linksFirstPart, Q, start, redirected(end), return_dict, event))
+                 args=(linksFirstPart, Q, start,
+                       end, return_list, 'Worker 1', run))
     p1.start()
 
     p2 = Process(target=pythonSlave.find_shortest_path,
-                 args=(linksSecondPart, Q, start, redirected(end), return_dict, event))
+                 args=(linksSecondPart, Q, start,
+                       end, return_list, 'Worker 2', run))
     p2.start()
 
     p3 = Process(target=pythonSlave.find_shortest_path,
-                 args=(linksThirdPart, Q, start, redirected(end), return_dict, event))
+                 args=(linksThirdPart, Q, start,
+                       end, return_list, 'Worker 3', run))
     p3.start()
 
     p4 = Process(target=pythonSlave.find_shortest_path,
-                 args=(linksFourthPart, Q, start, redirected(end), return_dict, event))
+                 args=(linksFourthPart, Q, start,
+                       end, return_list, 'Worker 4', run))
     p4.start()
 
     p5 = Process(target=pythonSlave.find_shortest_path,
-                 args=(linksFifthPart, Q, start, redirected(end), return_dict, event))
+                 args=(linksFifthPart, Q, start,
+                       end, return_list, 'Worker 5', run))
     p5.start()
 
     p1.join()
@@ -62,33 +45,78 @@ def processes(linksFirstPart, linksSecondPart, linksThirdPart, linksFourthPart, 
     p5.join()
 
 
-async def main():
-    event = multiprocessing.Event()
-    manager = multiprocessing.Manager()
-    return_dict = manager.dict()
+def printHelper(return_list, totalTime):
+    shortest = 9999999
+    shortestEntry = {}
+    helper = 1
 
-    start = "https://en.wikipedia.org/wiki/Airplane"
-    end = "https://en.wikipedia.org/wiki/Car"
-    path = {}
+    for a in return_list:  # processes outputs
+        if (len(a) < shortest):
+            shortest = len(a)
+            shortestEntry = a
+
+    print("\nFound a way from: " +
+          colored(str(shortestEntry[0]), 'blue') + " to: " + colored(str(shortestEntry[shortest-1]), 'blue') + " with " + str(shortest-2) + " pages between.")
+
+    print(colored("\nShortest route: \n", 'blue', attrs=['bold']))
+
+    for page in shortestEntry:
+        print(colored(str(helper) + ": "+str(page), 'cyan'))
+        helper += 1
+    print(colored("\nTotal time taken: " +
+                  str(round(totalTime, 2)) + " seconds.", 'green'))
+
+
+def main(start, end):
+    #run = Value('i', True)
+    manager = multiprocessing.Manager()
+    return_list = manager.list()
+    run = manager.Value('i', True)
+    # print(run)
+    path = manager.dict()
     path[start] = [start]
     Q = deque([start])
 
-    links = get_links(Q[0])
+    links = pythonSlave.get_links(Q[0])
     a = (round(len(links)/5))
     linksFirstPart = links[:a]
     linksSecondPart = links[a:2*a]
     linksThirdPart = links[2*a:3*a]
     linksFourthPart = links[3*a:4*a]
     linksFifthPart = links[4*a:]
-
+    Q = deque(
+        [start])
+    starttime = time.time()
     processes(linksFirstPart, linksSecondPart,
-              linksThirdPart, linksFourthPart, linksFifthPart, Q, start, end, return_dict, event)
+              linksThirdPart, linksFourthPart, linksFifthPart, Q, start, end, return_list, run)
+    endtime = time.time()
+    totalTime = endtime-starttime
+    printHelper(return_list, totalTime)
+
+
+def main2(start, end):
+
+    manager = multiprocessing.Manager()
+    return_list = manager.list()
+    run = manager.Value('i', True)
+
+    Q = deque([start])
+
+    links = pythonSlave.get_links(Q[0])
+
+    starttime = time.time()
+    p1 = Process(target=pythonSlave.find_shortest_path,
+                 args=(links, Q, start, end, return_list, 'process test',  run))
+    p1.start()
+    p1.join()
+    endtime = time.time()
+    totalTime = endtime-starttime
+    printHelper(return_list, totalTime)
+
 
 if __name__ == '__main__':
-    starttime = time.time()
 
-    asyncio.run(main())
-
-    endtime = time.time()
-
-    print(endtime - starttime)
+    start = "https://en.wikipedia.org/wiki/Airplane"
+    end = "https://en.wikipedia.org/wiki/Car"
+    main(start, end)
+    main2(start, end)
--- pythonSlave.py
+++ pythonSlave.py
@@ -5,47 +5,65 @@ import argparse
 from threading import Thread
 from bs4 import BeautifulSoup
 from collections import deque
+from termcolor import colored
+from multiprocessing import Process, Manager, Value
 
 
-def find_shortest_path(links, Q, start, end, return_dict, event):
-
-    print(Q)
+def find_shortest_path(links, Q, start, end, return_list, process, run):
+  # breadth first - search
+    print(process + " started. Finding shortest path...")
     i = 0
+    # (run.value)
+    # run = True
+    # print((links))
     path = {}
     path[start] = [start]
-
-    while len(Q) != 0:
-        # look at next page in queue of pages to visit, get wikilinks on that page
-        page = Q[0]
-        if i != 0:
+    page = Q[0]
+    while (run.value):
+        # look at next page in queue of pages to visit, get wikilinks from that page
+        if i != 0:  # we are given list so at first iteration don't get new links
             page = Q.popleft()
             links = get_links(page)
-        i = 1
+
         # look at each link on the page
+        # print(len(links))
         for link in links:
 
             # if link is our destination, we're done!
-            if link in end:
-                print(path[page] + [link])
-                return_dict["ass"] = path[page] + [link]
-                event.set()
-                return return_dict
-            # if not, check if we already have a record of the shortest path from the start page to this link- if we don't, we need to record the path and add the link to our queue of pages to explore
+            if link == end:
+                # link is end.
+                print(process + " : " + colored("Found a way to our destination...",
+                                                'green'))
+                return_list.append(path[page] + [link])
+                run.value = False
+                # print(run)
+                return return_list
+            # if not, check if we already have a record of the shortest path from the start page to this link-
+            # if we don't, we need to record the path and add the link to our queue of pages to explore
             if (link not in path) and (link != page):
                 path[link] = path[page] + [link]
+                if i == 0:
+                    Q.append(link)  # adds to right side
+                    Q.popleft()  # pop left, has the [start] in it
+                    i = 1
+
                 Q.append(link)
 
-    # if we've exhausted all possible pages to explore in our queue without getting to the destination
+    print(process + " returning.")
     return None
 
 
 def get_links(page):
-    '''
-    Retrieves distinct links in a Wikipedia page.
-    '''
-    r = requests.get(page)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    base_url = page[:page.find('/wiki/')]
-    links = list({base_url + a['href']
-                  for a in soup.select('p a[href]') if a['href'].startswith('/wiki/')})
+    if page == "https://en.wikipedia.org/wiki/Kosmoceratops":
+        print("jeps")
+
+    links = []
+    request = requests.get(page)
+    soup = BeautifulSoup(request.content, 'html.parser')
+    baseUrl = "https://en.wikipedia.org"
+
+    for link in soup.select('p a[href]'):  # select links
+        if link['href'].startswith('/wiki/'):  # if link is valid
+            links.append(baseUrl + link['href'])  # add to list
+
     return links
